268fd6acae6d51fb06804d307661a700705f1323
angrymiester <anikethaldankar73@gmail.com>
2022-08-15 16:44:57 +0530

    Day 10 Completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,23 @@
+class item {
+  final String id;
+  final String name;
+  final String Desc;
+  final num price;
+  final String colour;
+  final String image;
+
+  item({this.id, this.name, this.Desc, this.price, this.colour, this.image});
+}
+
+final products = [
+
+  item(
+      id: "Aniket001",
+      name: "iphone 12 pro",
+      Desc: "3rd genration apple ",
+      price: 999,
+      colour: "#33505a"
+
+  )
+];
+
