c3fee103c431aefd3171a178823f6ec45fe4b1b8
angrymiester <anikethaldankar73@gmail.com>
2022-08-15 10:36:44 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'package:flutter_application_1/home_page.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      home: HomePage()
+    );
   }
 }
